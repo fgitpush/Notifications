6fa314f7ca890d640c0b06a68c0a9c35c586ffb6
Faad Uddin <ifahaduddin@gmail.com>
2020-04-15 18:18:38 +0500

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+  
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+use App\User;
+use Notification;
+use App\Notifications\MyFirstNotification;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+  
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+  
+    public function sendNotification()
+    {
+        
+        $user = Auth::user();
+  
+        $details = [
+            'greeting' => 'Hi Artisan',
+            'body' => 'This is my first notification from ItSolutionStuff.com',
+            'thanks' => 'Thank you for using ItSolutionStuff.com tuto!',
+            'actionText' => 'View My Site',
+            'actionURL' => url('/'),
+            'order_id' => 101
+        ];
+  
+        Notification::send($user, new MyFirstNotification($details));
+   
+        dd('done');
+    }
+  
+}
